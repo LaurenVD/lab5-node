c8a16ae34cd193903c1dfdeb0d8c493d363a8a57
LaurenV <laurenvdl45@gmail.com>
2022-10-20 17:21:15 +0200

    feat: check first route

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// start file can be found on https://expressjs.com/en/starter/hello-world.html
-
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(["appel", "peer", "banaan"])
+})
+
+app.get('/api/v1/messages', (req, res) => {
+  res.send("Hello World")
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
