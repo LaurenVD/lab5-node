67cd80f65343b72e2960aa333421975504c795bd
LaurenV <laurenvdl45@gmail.com>
2022-10-20 20:48:51 +0200

    feat: add message moddel

---
+++ models/message.js
@@ -0,0 +1,16 @@
+//require the mongoose module
+const mongoose = require('mongoose');
+//define Schema variable
+const Schema = mongoose.Schema;
+
+//define message schema
+const messageSchema = new Schema({
+    user: { type: String, required: true },
+    message: { type: String, required: true },
+});
+
+//define message model
+const Message = mongoose.model('Message', messageSchema);
+
+//export message model
+module.exports = Message;
