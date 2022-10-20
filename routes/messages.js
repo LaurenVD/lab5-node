32f32a002f8b7479ba3aa8cf7462416a7a8153b0
LaurenV <laurenvdl45@gmail.com>
2022-10-20 17:28:19 +0200

    feat: add folders for routers

--- routes/messages.js
+++
@@ -1,8 +0,0 @@
-// require express router
-const express = require('express')
-const router = express.Router()
-// require the messages controller
-const messagesController = require('../controllers/messages')
-
-// get all messages
-router.get('/', messagesController.getAll)
