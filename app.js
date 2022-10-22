581725cc77a4654ab08fd4b86a32cf92cbafcb68
LaurenV <laurenvdl45@gmail.com>
2022-10-22 17:24:49 +0200

    feat: add index page

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express()
 const port = 3000
 const logger = require('./middleware/logger')
 const apiV1Messages = require('./routers/api/v1/messages')
+const indexRouter = require('./routers/index')
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/lab5');
 
-app.get('/', (req, res) => {
-  res.send(["appel", "peer", "banaan"])
-})
+app.set('view engine', 'jade');
+
+app.use('/', indexRouter);
 
 //json
 app.use(express.json());
